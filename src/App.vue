c833e4881fde2650f269fef34b422a90d8e0009a
jonehoo <617464665@qq.com>
2022-07-23 10:23:50 +0800

    E-PAINT.DWH

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+<style>
+body{
+  margin: 0px;
+  
+}
+#app{
+  height: 100vh;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    gffgfgfgfgfg
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'HomeView',
+
+}
+</script>
